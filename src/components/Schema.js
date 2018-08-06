107db1b53c04386675a143bcdf8dcfeffd92336f
Steve Urmston <steve.urmston@bytemark.co.uk>
2018-08-06 10:01:42 +0100

    :bug: Fix for Schema component

--- src/components/Schema.js
+++ src/components/Schema.js
@@ -1,34 +1,38 @@
-import React from 'react'
+import React, { PureComponent } from 'react'
 import { JSONLD, Generic } from 'react-structured-data'
 
-const Schema = () => (
-  <JSONLD>
-    <Generic
-      type="person"
-      jsonldtype="Person"
-      schema={
-        {
-          "email": "steve@urm.st",
-          "familyName": "Urmston",
-          "givenName": "Steve",
-          "jobTitle": "Full Stack Digital Product Designer"
-        }
-      }>
-      <Generic
-        type="address"
-        jsonldtype="PostalAddress"
-        schema={
-          {
-            "addressCountry": "GB",
-            "addressLocality": "York",
-            "postalCode": "YO10 4QE",
-            "streetAddress": "The Coach House, Fulford Park",
-            "name": "Steve Urmston"
-          }
-        }
-      />
-    </Generic>
-  </JSONLD>
-)
+class Schema extends PureComponent {
+  render () {
+    return (
+      <JSONLD>
+        <Generic
+          type="person"
+          jsonldtype="Person"
+          schema={
+            {
+              "email": "steve@urm.st",
+              "familyName": "Urmston",
+              "givenName": "Steve",
+              "jobTitle": "Full Stack Digital Product Designer"
+            }
+          }>
+          <Generic
+            type="address"
+            jsonldtype="PostalAddress"
+            schema={
+              {
+                "addressCountry": "GB",
+                "addressLocality": "York",
+                "postalCode": "YO10 4QE",
+                "streetAddress": "The Coach House, Fulford Park",
+                "name": "Steve Urmston"
+              }
+            }
+          />
+        </Generic>
+      </JSONLD>
+    )
+  }
+}
 
 export default Schema
