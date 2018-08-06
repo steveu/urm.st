1dbbf3ad96f557eb457137486db5179bab4aed28
Steve Urmston <steve.urmston@bytemark.co.uk>
2018-08-06 01:25:51 +0100

    :racehorse: Add about images directory to public for optimisation

--- src/containers/About.js
+++ src/containers/About.js
@@ -1,8 +1,6 @@
 import React from 'react'
-import { withRouteData, Link, Head } from 'react-static'
+import { withRouteData, Head } from 'react-static'
 import convert from 'htmr'
-import steve from '../images/steve.jpg'
-import steveLandscape from '../images/steve-landscape.jpg'
 
 export default withRouteData(({ about }) => (
   <div>
@@ -13,11 +11,10 @@ export default withRouteData(({ about }) => (
       <div className="grid-wrapper about">
         <figure className="about__image">
           <picture>
-            <source media="(min-width: 34em)" srcSet={steve} />
-            <source media="(min-width: 0px)" srcSet={steveLandscape} />
-            <img src={steve} alt="Steve Urmston - Digital Product Designer" />
+            <source media="(min-width: 34em)" srcSet="/assets/images/steve.jpg" />
+            <source media="(min-width: 0px)" srcSet="/assets/images/steve-landscape.jpg" />
+            <img src="/assets/images/steve.jpg" alt="Steve Urmston - Digital Product Designer" />
           </picture>
-
         </figure>
         <aside>
           {convert(about.contents)}
