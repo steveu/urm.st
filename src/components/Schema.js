7e7da51bc254ee0fc431262dfb78f2581579cdee
Steve Urmston <steve.urmston@bytemark.co.uk>
2018-08-06 10:03:22 +0100

    Revert ":bug: Fix for Schema component"
    This reverts commit 107db1b53c04386675a143bcdf8dcfeffd92336f.

--- src/components/Schema.js
+++ src/components/Schema.js
@@ -1,38 +1,34 @@
-import React, { PureComponent } from 'react'
+import React from 'react'
 import { JSONLD, Generic } from 'react-structured-data'
 
-class Schema extends PureComponent {
-  render () {
-    return (
-      <JSONLD>
-        <Generic
-          type="person"
-          jsonldtype="Person"
-          schema={
-            {
-              "email": "steve@urm.st",
-              "familyName": "Urmston",
-              "givenName": "Steve",
-              "jobTitle": "Full Stack Digital Product Designer"
-            }
-          }>
-          <Generic
-            type="address"
-            jsonldtype="PostalAddress"
-            schema={
-              {
-                "addressCountry": "GB",
-                "addressLocality": "York",
-                "postalCode": "YO10 4QE",
-                "streetAddress": "The Coach House, Fulford Park",
-                "name": "Steve Urmston"
-              }
-            }
-          />
-        </Generic>
-      </JSONLD>
-    )
-  }
-}
+const Schema = () => (
+  <JSONLD>
+    <Generic
+      type="person"
+      jsonldtype="Person"
+      schema={
+        {
+          "email": "steve@urm.st",
+          "familyName": "Urmston",
+          "givenName": "Steve",
+          "jobTitle": "Full Stack Digital Product Designer"
+        }
+      }>
+      <Generic
+        type="address"
+        jsonldtype="PostalAddress"
+        schema={
+          {
+            "addressCountry": "GB",
+            "addressLocality": "York",
+            "postalCode": "YO10 4QE",
+            "streetAddress": "The Coach House, Fulford Park",
+            "name": "Steve Urmston"
+          }
+        }
+      />
+    </Generic>
+  </JSONLD>
+)
 
 export default Schema
