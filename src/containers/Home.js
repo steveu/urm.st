00ce519efe26b3026eb513f2c53488d7e099117a
Steve Urmston <steve.urmston@bytemark.co.uk>
2018-09-03 10:26:58 +0100

    Revert "Revert ":sparkles: Add animation to route change. Fix duplicate meta descriptions""
    This reverts commit 504230e9443afb216d2796e93e494cc73a72f74d.

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -1,47 +1,20 @@
 import React from 'react'
 import { withRouteData, Head } from 'react-static'
 import convert from 'htmr'
-import Projects from '../components/Projects'
+
+import AnimatedRoute from '../components/AnimatedRoute'
+import Home from '../components/Home'
+const AnimatedHome = AnimatedRoute(Home);
 
 export default withRouteData(({ home, projects }) => (
   <div>
     <Head>
-      <title>Steve Urmston | Digital Product Designer</title>
+      <title>Steve Urmston | Full Stack Digital Product Designer</title>
+      <meta name="description" content="Personal portolio of Steve Urmston, a Digital Product Designer based in York, UK" />
     </Head>
-    <section className="skills">
-      <div className="grid-wrapper">
-        {convert(home.contents)}
-        <ul>
-          <li>
-            <h4>Product Management</h4>
-            <p>Strategy, Business, Leadership</p>
-          </li>
-          <li>
-            <h4>User Experience Design</h4>
-            <p>Research and User Testing</p>
-          </li>
-          <li>
-            <h4>UI & Interaction Design</h4>
-            <p>For Web and Mobile</p>
-          </li>
-          <li>
-            <h4>JavaScript Development</h4>
-            <p>React, AngularJS, Node</p>
-          </li>
-          <li>
-            <h4>Responsive Web Design</h4>
-            <p>HTML, CSS, Sass</p>
-          </li>
-          <li>
-            <h4>Digital Marketing</h4>
-            <p>Analytics, CRO and SEO</p>
-          </li>
-        </ul>
-      </div>
-    </section>
-    <Projects
+    <AnimatedHome
+      contents={convert(home.contents)}
       projects={projects}
-      isFeatured="true"
-    ></Projects>
+    />
   </div>
 ))
